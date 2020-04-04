34b1854c0d22fef7473edf741d5a374c0d679816
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-04-04 12:29:08 +0200

    Fixed Version By Crow Balde

---
+++ src/Crow/HealPlugin/healing.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Crow\HealPlugin;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class healing extends PluginBase{
+
+	public function onEnable()
+	{		
+		$this->getLogger()->info("HealPlugin aktiviert!");
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args)
+	{
+		switch ($cmd->getName()) {
+			case "heal":
+				if($sender instanceof Player) {
+					$sender->sendMessage(TextFormat::GREEN . "Du Hast Dich Erfolgreich Geheilt");
+					$sender->setHealth(20);
+					return true;
+				} else{
+					$sender->sendMessage(TextFormat::RED . "Du brauchst denn rang, Supreme oder Premium Um dich zu heilen");
+					return true;
+				}
+		}
+	}
+}
