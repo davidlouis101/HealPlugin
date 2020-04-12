6d87b993148c765dca45ebdd82494e153105fd38
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-04-12 14:35:23 +0200

    Update healing.php

--- src/Crow/HealPlugin/healing.php
+++ src/Crow/HealPlugin/healing.php
@@ -8,7 +8,7 @@ use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
-class Healing extends PluginBase{
+class Main extends PluginBase{
 
 	public function onEnable()
 	{		
